f8c9b450a05bd2d33634f15685f4bdadf4fde5c0
Vishaal <vishaal16119@iiitd.ac.in>
2018-05-18 19:30:54 +0530

    Added getting terminal window size easy

--- vkte.c
+++ vkte.c
@@ -4,12 +4,24 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <errno.h>
+#include <sys/ioctl.h>
 
 #define CTRL_Q 17
 
-struct termios original;
+//to store terminal configurations
+struct terminal_editor_configuration
+{
+	int rows;
+	int columns;
+	struct termios original;
+};
+
+struct terminal_editor_configuration terminal;
 
 void kill_process(char*);
+void draw_line_numbers();
+int get_terminal_size(int*, int*);
+void set_terminal_size();
 
 char return_keypress()
 //wait for character input
@@ -32,6 +44,50 @@ void clear_screen()
 		kill_process("write");
 	if(write(1, "\x1b[1;1H", 3)==-1)
 		kill_process("write");
+
+	draw_line_numbers();
+
+	if(write(1, "\x1b[1;1H", 3)==-1)
+		kill_process("write");
+
+}
+
+int get_terminal_size(int *r, int *c)
+{
+	struct winsize size;
+
+	if(ioctl(1, TIOCGWINSZ, &size)==-1)
+	{
+		return -1;
+	}
+	else if(size.ws_col==0)
+	{
+		return -1;
+	}
+	else
+	{
+		*r=size.ws_row;
+		*c=size.ws_col;
+		return 0;
+	}
+}
+
+void set_terminal_size()
+{
+	if(get_terminal_size(&terminal.rows, &terminal.columns)==-1)
+		kill_process("get_terminal_size");
+}
+
+void draw_line_numbers()
+{
+	int line_number=1;
+
+	for(line_number=1;line_number<=terminal.rows;line_number++)
+	{
+		//char buffer[4];
+		//sprintf(buffer, "%d\r\n", line_number);
+		write(1, "~\r\n", 3);
+	}
 }
 
 void handle_keypress()
@@ -91,7 +147,7 @@ void switch_to_canonical_mode(struct termios orig)
 void switch_to_canonical_mode_atexit()
 //switches input mode back to canonical mode(atexit)
 {
-	if(tcsetattr(0, TCSAFLUSH, &original)==-1)
+	if(tcsetattr(0, TCSAFLUSH, &terminal.original)==-1)
 		kill_process("tcsetattr");
 }
 
@@ -101,10 +157,11 @@ int main(int argc, char const *argv[])
 	struct termios original_mode;
 	if(tcgetattr(0, &original_mode)==-1)
 		kill_process("tcgetattr");
-	if(tcgetattr(0, &original)==-1)
+	if(tcgetattr(0, &terminal.original)==-1)
 		kill_process("tcgetattr");;
 	atexit(switch_to_canonical_mode_atexit);
 	switch_to_raw_mode();
+	set_terminal_size();
 
 	char inp;
 	while(1)
